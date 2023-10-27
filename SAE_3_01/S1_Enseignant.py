ef12fbe2727f11954d9143cefa738438a49801c6
YoYo <yg19072004@gmail.com>
2023-10-27 13:14:29 +0200

    avancer le code pour ecrire dans l'excel

--- SAE_3_01/S1_Enseignant.py
+++ SAE_3_01/S1_Enseignant.py
@@ -1,13 +1,16 @@
 import sqlite3
 import openpyxl
 
+# Demander à l'utilisateur la ressource à rechercher
+resource = input("Entrez la ressource que vous souhaitez écrire dans le fichier Excel : ")
+
 # Connexion à la base de données SQLite
 try:
     with sqlite3.connect("database/database.db") as conn:
         cursor = conn.cursor()
 
-        # Exécutez une requête SQL pour récupérer les données depuis la base de données
-        cursor.execute("SELECT Libelle, H_CM, H_TD, H_TP FROM Maquette WHERE Libelle LIKE '%R1.01%'")
+        # Exécutez une requête SQL pour récupérer les données depuis la base de données en utilisant la ressource fournie par l'utilisateur
+        cursor.execute("SELECT Ressource, H_CM, H_TD, H_TP, Resp FROM Planning WHERE Ressource LIKE ?", ('%' + resource + '%',))
 
         # Récupérez les données dans une liste de tuples
         data_from_database = cursor.fetchall()
@@ -27,23 +30,23 @@ try:
         col_h_td = 3
         col_h_tp = 4
 
-        # Récupère les valeur de la requetes SQL écrire les données spécifiques à la ligne 5, colonnes 2, 3 et 4
+        # Récupère les valeurs de la requête SQL et écrire les données spécifiques dans le fichier Excel
         worksheet.cell(row=row, column=col_h_cm, value=data_from_database[0][1])
         worksheet.cell(row=row, column=col_h_td, value=data_from_database[0][2])
         worksheet.cell(row=row, column=col_h_tp, value=data_from_database[0][3])
 
-        # Écrire "R1.01 Initiation au développement" dans la colonne 2, ligne 2
-        worksheet.cell(row=2, column=2, value=data_from_database[0][0])
+        # Écrire la ressource fournie par l'utilisateur dans la colonne 2, ligne 2
+        worksheet.cell(row=2, column=2, value=resource)
 
-        # Écrire "test" dans la colonne 7, ligne 2 à remplacer par la suite avec la base de donnée pour le responsable
-        worksheet.cell(row=2, column=7, value="test")
+        # Écrire le responsable dans la colonne 7, ligne 2
+        worksheet.cell(row=2, column=7, value=data_from_database[0][4])
 
         # Sauvegarder le classeur Excel
         workbook.save("S2.xlsx")
 
-        # Message pour comfirmer les ajouts dans le fichier excel
-        print("Les données ont été ajoutées à S2.xlsx")
+        # Message pour confirmer les ajouts dans le fichier Excel
+        print(f"Les données pour la ressource '{resource}' ont été ajoutées à S2.xlsx")
 
-# Si il y a un problème lors de la connexion on affiche un message d'erreur
+# Si une erreur survient lors de la connexion à la base de données, affichez un message d'erreur
 except sqlite3.Error as e:
     print(f"Erreur lors de la connexion à la base de données : {e}")
