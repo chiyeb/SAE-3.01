4c6ffa4f83153aa30079bd58767e7274f86a6c09
YoYo <yg19072004@gmail.com>
2023-12-13 15:44:46 +0100

    ecrire dans l'excel tout le S1

--- SAE_3_01/S1_Enseignant.py
+++ SAE_3_01/S1_Enseignant.py
@@ -1,51 +1,52 @@
 import sqlite3
 import openpyxl
 
-resource = "R1.01"
-
 # Connexion à la base de données SQLite
 try:
     with sqlite3.connect("database/database.db") as conn:
         cursor = conn.cursor()
 
-        # Exécute une requête SQL pour récupérer les données depuis la base de données en utilisant la ressource fournie
-        cursor.execute("SELECT Ressource, H_CM, H_TD, H_TP, Resp FROM Planning WHERE Ressource LIKE ?", ('%' + resource
-                                                                                                         + '%',))
+        # Exécutez une requête SQL pour récupérer toutes les ressources depuis la base de données
+        cursor.execute("SELECT DISTINCT Ressource FROM Planning")
 
-        # Récupérez les données dans une liste de tuples
-        data_from_database = cursor.fetchall()
+        # Récupérez toutes les ressources dans une liste
+        resources = [row[0] for row in cursor.fetchall()]
 
-        # Charger un classeur Excel existant s'il existe, sinon créer un nouveau
+        # Chuarger un classeur Excel existant s'il existe, sinon créer un noveau
         try:
-            workbook = openpyxl.load_workbook("Documents/S2.xlsx")
+            workbook = openpyxl.load_workbook("S1.xlsx")
         except FileNotFoundError:
-            workbook = openpyxl.Workbook()
+            print("Le fichier S1.xlsx n'a pas été trouvé.")
+            exit()
 
-        # Sélectionner la feuille active
-        worksheet = workbook.active
+        for resource in resources:
+            # Créer une nouvelle feuille avec le nom de la ressource
+            worksheet = workbook.create_sheet(title=resource)
 
-        # Définir les lignes et colonnes correspondantes
-        row = 5
-        col_h_cm = 2
-        col_h_td = 3
-        col_h_tp = 4
+            # Copier toutes les données de la feuille de base (S2.xlsx) dans la nouvelle feuille
+            base_sheet = workbook["Feuil1"]
+            for row in base_sheet.iter_rows(min_row=1, max_col=base_sheet.max_column, max_row=base_sheet.max_row):
+                worksheet.append([cell.value for cell in row])
 
-        # Récupère les valeurs de la requête SQL et écrire les données spécifiques dans le fichier Excel
-        worksheet.cell(row=row, column=col_h_cm, value=data_from_database[0][1])
-        worksheet.cell(row=row, column=col_h_td, value=data_from_database[0][2])
-        worksheet.cell(row=row, column=col_h_tp, value=data_from_database[0][3])
+            # Exécutez une requête SQL pour récupérer les données de la ressource actuelle
+            cursor.execute("SELECT Ressource, H_CM, H_TD, H_TP, Resp FROM Planning WHERE Ressource = ?", (resource,))
 
-        # Écrire la ressource fournie par l'utilisateur dans la colonne 2, ligne 2
-        worksheet.cell(row=2, column=2, value=resource)
+            # Récupérez les données dans une liste de tuples
+            data_from_database = cursor.fetchall()
 
-        # Écrire le responsable dans la colonne 7, ligne 2
-        worksheet.cell(row=2, column=7, value=data_from_database[0][4])
+            # Écrire les données spécifiques dans la nouvelle feuille
+            worksheet.cell(row=5, column=2, value=data_from_database[0][1])
+            worksheet.cell(row=5, column=3, value=data_from_database[0][2])
+            worksheet.cell(row=5, column=4, value=data_from_database[0][3])
+            worksheet.cell(row=2, column=2, value=resource)
+            worksheet.cell(row=2, column=7, value=data_from_database[0][4])
+
 
         # Sauvegarder le classeur Excel
-        workbook.save("S2.xlsx")
+        workbook.save("S1.xlsx")
 
         # Message pour confirmer les ajouts dans le fichier Excel
-        print(f"Les données pour la ressource '{resource}' ont été ajoutées à S2.xlsx")
+        print("Les données ont été ajoutées à S1.xlsx")
 
 # Si une erreur survient lors de la connexion à la base de données, affichez un message d'erreur
 except sqlite3.Error as e:
