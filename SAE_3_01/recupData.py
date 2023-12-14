d922b59354936c416c13aeb3942191595346b15e
NGUYEN-Lucas-2225063a <lucas.NGUYEN@etu.univ-amu.fr>
2023-12-14 23:20:36 +0100

    Udpate

--- SAE_3_01/recupData.py
+++ SAE_3_01/recupData.py
@@ -1,6 +1,6 @@
 import sqlite3
 import pandas as pd
-
+from openpyxl.reader.excel import load_workbook
 
 from insertData import insertData
 
@@ -88,6 +88,10 @@ class recupData:
         # Lecture du fichier Excel en dehors de la boucle pour éviter de le charger à chaque itération
         S = pd.read_excel(horaire, semestre_onglet)
 
+        # Récupérer les couleurs des cellules
+        sheet_name = semestre_onglet
+        cell_colors = self.get_cell_colors(horaire, sheet_name)
+
         # Recherche du mot "Date" dans les colonnes
         for col in S.columns:
             if 'Date' in col:
@@ -100,22 +104,23 @@ class recupData:
                         index_date = S[S[col] == date].index[0]
 
                         # Utilise la méthode iloc pour accéder à la ligne complète
-                        row_data = S.iloc[index_date, :].tolist()
+                        row_data = S.iloc[index_date, 2:].tolist()
 
                         # Supprime les valeurs NaN
-                        row_data = [x for x in row_data if pd.notna(x)]
+                        #row_data = [x for x in row_data if pd.notna(x)]
 
                         # Supprime les valeurs vides
-                        row_data = [x for x in row_data if x != ""]
+                        #row_data = [x for x in row_data if x != ""]
 
                         # Filtrer seulement les valeurs "X" et "Y"
-                        row_data = [x for x in row_data if 'X' in str(x) or 'Y' in str(x)]
+                        #row_data = [x for x in row_data if 'X' in str(x) or 'Y' in str(x)]
 
-                        #Afficher un message d'erreur si la ligne est vide
+
+
+                        # Afficher un message d'erreur si la ligne est vide
                         if not row_data:
                             print(f"Dans la colonne 'Date', la ligne {index_date} est vide")
                             continue
-
 
                         # Créer une fonction de mapping pour attribuer les labels "TD" ou "TP"
                         def map_label(value):
@@ -127,29 +132,47 @@ class recupData:
                                 return value
 
                         # Appliquer la fonction de mapping à la colonne
-                        row_data = list(map(map_label, row_data))
+                        #row_data = list(map(map_label, row_data))
 
                         print(f"Date: {date}, Ligne complète: {row_data}")
 
-                        # Appliquer le style pour afficher la couleur de fond
-                        styled_row_data = S.iloc[index_date, :].apply(
-                            lambda x: f'background-color: {x}')
-
-                        # Appliquer le style pour afficher la couleur de fond que pour les valeurs "X" et "Y"
-                        styled_row_data = styled_row_data.apply(
-                            lambda x: f'background-color: {x}' if 'X' in str(x) or 'Y' in str(x) else '')
-
-                        # Supprimer les lignes où il n'y a pas de background-color
-                        styled_row_data = styled_row_data[styled_row_data != '']
-
-                        # Appliquer la fonction de mapping à la colonne
-                        styled_row_data = styled_row_data.apply(map_label)
-
-                        # Afficher la ligne avec le style de couleur de fond de la cellule
-                        print(styled_row_data)
+                        # Afficher les noms des couleurs des cellules de chaque ligne
+                        for j, cell_value in enumerate(row_data):
+                            cell_coord = (index_date + 2, j + 3)
+                            cell_color = cell_colors[cell_coord]
+                            print(f"Coordonnées : {cell_coord} - Valeur : {cell_value}, Couleur : {cell_color}")
 
                     else:
                         print(f"La date {date} n'a pas été trouvée dans la colonne {col}")
+
+    def get_cell_colors(self, horaire, sheet_name):
+        # Chargement du classeur Excel
+        workbook = load_workbook(horaire, data_only=True)
+
+        # Sélection de la feuille de calcul
+        sheet = workbook[sheet_name]
+
+        # Dictionnaire pour stocker les couleurs des cellules
+        cell_colors = {}
+        # Parcours de toutes les lignes de row_data
+        for row_data in sheet.iter_rows():
+            # Parcours de toutes les cellules de la ligne
+            for cell in row_data:
+                # Récupère la couleur de la cellule
+                cell_color = cell.fill.start_color.index
+
+                # Récupère les coordonnées de la cellule
+                cell_coord = (cell.row, cell.column)
+
+                # Ajoute la couleur de la cellule au dictionnaire
+                cell_colors[cell_coord] = cell_color
+
+        return cell_colors
+
+
+
+
+
 
     def __del__(self):
         # Ferme la connexion à la base de données lorsque l'objet est détruit
