6628ab9fbe0b52f864c99e24e78a16cb95694374
Tymeo Rugiero <115974019+RENAY-RUGIERO-Tymeo-2225070a@users.noreply.github.com>
2023-12-17 21:50:03 +0100

    bd

--- SAE_3_01/recupHProf.py
+++ SAE_3_01/recupHProf.py
@@ -1,7 +1,8 @@
 import pandas as pd
+from sqlalchemy import create_engine
 
 
-def extract_and_display_resource_info(df):
+def Heures_ressources(df):
     """
     Extract, structure, and display the resource information from the DataFrame.
 
@@ -20,12 +21,10 @@ def extract_and_display_resource_info(df):
         tp_dedoubles = row['TP (dédoublés)']
         test = row['Test']
 
-        # Check if new resource starts
         if pd.notna(resource):
             current_resource = resource
             structured_data[current_resource] = []
 
-        # Check if row contains intervenant data
         if pd.notna(intervenant):
             structured_data[current_resource].append({
                 'Intervenant': intervenant,
@@ -36,36 +35,56 @@ def extract_and_display_resource_info(df):
                 'Test': test
             })
 
-    # Display the structured data
+    return structured_data
+
+
+def create_db_connection():
+    # SQLite database file
+    db_file = 'database.db'
+
+    # Creating a SQLite database engine
+    engine = create_engine(f'sqlite:///{db_file}')
+
+    # Returning the database engine
+    return engine
+
+
+def insert_data_into_db(engine, structured_data):
+    # Establishing a database connection
+    connection = engine.connect()
+
+    # Iterating through the structured data and inserting into the database
     for resource, data in structured_data.items():
-        print(f"### Ressource : {resource}")
-        if data:
-            for d in data:
-                print(f"- **Intervenant : {d['Intervenant']}**")
-                print(f"  - CM : {d['CM']} heure" if pd.notna(d['CM']) else "  - CM : Non spécifié")
-                print(f"  - TD : {d['TD']} heure" if pd.notna(d['TD']) else "  - TD : Non spécifié")
-                print(f"  - TP (non dédoublés) : {d['TP (non dédoublés)']} heure" if pd.notna(
-                    d['TP (non dédoublés)']) else "  - TP (non dédoublés) : Non spécifié")
-                print(f"  - TP (dédoublés) : {d['TP (dédoublés)']} heure" if pd.notna(
-                    d['TP (dédoublés)']) else "  - TP (dédoublés) : Non spécifié")
-                print(f"  - Test : {d['Test']} heure" if pd.notna(d['Test']) else "  - Test : Non spécifié")
-        else:
-            print("  Aucune donnée pour cette ressource pour le moment.")
-        print("\n")
+        for d in data:
+            connection.execute('''
+                INSERT INTO HeureRessource (NomRessource, NomIntervenants, hTD, hTP, hCM, hTEST)
+                VALUES (?, ?, ?, ?, ?, ?, ?)
+            ''', (
+                resource,
+                d['Intervenant'],
+                d['CM'],
+                d['TD'],
+                d['TP (non dédoublés)'],
+                d['TP (dédoublés)'],
+                d['Test']
+            ))
+
+    # Closing the database connection
+    connection.close()
 
 
-# Load the Excel file
 file_path = 'QuiFaitQuoi_beta.xlsx'
 excel_data = pd.ExcelFile(file_path)
 
-# Display the sheet names and the first few rows of each sheet to understand the structure
 sheet_names = excel_data.sheet_names
-sheets_preview = {}
+structured_data = {}
 
 for sheet in sheet_names:
     df = pd.read_excel(file_path, sheet_name=sheet)
-    sheets_preview[sheet] = df.head()
-df_s1 = pd.read_excel(file_path, sheet_name=sheet_names[0])
+    structured_data.update(Heures_ressources(df))
 
-# Extract and display the resource information
-extract_and_display_resource_info(df_s1)
+# Creating a database connection
+engine = create_db_connection()
+
+# Inserting data into the 'resources' table
+insert_data_into_db(engine, structured_data)
