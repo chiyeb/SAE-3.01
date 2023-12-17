969b7371310ea55de383ec1a83a4a1ef48a2984c
NGUYEN-Lucas-2225063a <lucas.NGUYEN@etu.univ-amu.fr>
2023-12-17 20:32:52 +0100

    Udpate recupHProf.py

--- SAE_3_01/recupHProf.py
+++ SAE_3_01/recupHProf.py
@@ -1,7 +1,13 @@
 import pandas as pd
+import sqlite3
 
 
-def extract_and_display_resource_info(df):
+# Établir la connexion à la base de données (cela crée également le fichier de base de données s'il n'existe pas)
+conn = sqlite3.connect('database/database.db')
+
+# Créer un objet curseur pour exécuter des requêtes SQL
+cursor = conn.cursor()
+def extract_and_display_resource_info(df, conn):
     """
     Extract, structure, and display the resource information from the DataFrame.
 
@@ -23,12 +29,24 @@ def extract_and_display_resource_info(df):
         # Check if new resource starts
         if pd.notna(resource):
             current_resource = resource
-            structured_data[current_resource] = []
+            structured_data[current_resource] = {}
+
+            cursor.execute('SELECT COUNT(*) FROM RecupHProf WHERE ressource = ?', (current_resource,))
+            resource_count = cursor.fetchone()[0]
+
+            if resource_count == 0:
+                # If the resource does not exist, insert it
+                cursor.execute('INSERT INTO RecupHProf (ressource) VALUES (?)', (current_resource,))
+                conn.commit()
 
         # Check if row contains intervenant data
         if pd.notna(intervenant):
-            structured_data[current_resource].append({
-                'Intervenant': intervenant,
+            if current_resource not in structured_data:
+                structured_data[current_resource] = {}
+            if intervenant not in structured_data[current_resource]:
+                structured_data[current_resource][intervenant] = []
+
+            structured_data[current_resource][intervenant].append({
                 'CM': cm,
                 'TD': td,
                 'TP (non dédoublés)': tp_non_dedoubles,
@@ -37,21 +55,38 @@ def extract_and_display_resource_info(df):
             })
 
     # Display the structured data
-    for resource, data in structured_data.items():
-        print(f"### Ressource : {resource}")
-        if data:
-            for d in data:
-                print(f"- **Intervenant : {d['Intervenant']}**")
-                print(f"  - CM : {d['CM']} heure" if pd.notna(d['CM']) else "  - CM : Non spécifié")
-                print(f"  - TD : {d['TD']} heure" if pd.notna(d['TD']) else "  - TD : Non spécifié")
-                print(f"  - TP (non dédoublés) : {d['TP (non dédoublés)']} heure" if pd.notna(
-                    d['TP (non dédoublés)']) else "  - TP (non dédoublés) : Non spécifié")
-                print(f"  - TP (dédoublés) : {d['TP (dédoublés)']} heure" if pd.notna(
-                    d['TP (dédoublés)']) else "  - TP (dédoublés) : Non spécifié")
-                print(f"  - Test : {d['Test']} heure" if pd.notna(d['Test']) else "  - Test : Non spécifié")
-        else:
-            print("  Aucune donnée pour cette ressource pour le moment.")
-        print("\n")
+    for resource, intervenant_data in structured_data.items():
+        print(f"### Ressource : {resource} ###")
+
+        for intervenant, data_list in intervenant_data.items():
+            print(f"Ressource: {resource} - Intervenant : {intervenant} ")
+
+            if data_list:
+                for d in data_list:
+                    print(f"    - CM : {d['CM']} heure" if pd.notna(d['CM']) else "    - CM : Non spécifié")
+                    print(f"    - TD : {d['TD']} heure" if pd.notna(d['TD']) else "    - TD : Non spécifié")
+                    print(f"    - TP (non dédoublés) : {d['TP (non dédoublés)']} heure" if pd.notna(
+                        d['TP (non dédoublés)']) else "    - TP (non dédoublés) : Non spécifié")
+                    print(f"    - TP (dédoublés) : {d['TP (dédoublés)']} heure" if pd.notna(
+                        d['TP (dédoublés)']) else "    - TP (dédoublés) : Non spécifié")
+                    print(f"    - Test : {d['Test']} heure" if pd.notna(d['Test']) else "    - Test : Non spécifié")
+
+                    cursor.execute("INSERT INTO RecupHProf (Ressource, Intervenant, CM, TD, TP_non_dedoubles, TP_dedoubles, Test) VALUES (?, ?, ?, ?, ?, ?, ?)",
+                        (resource, intervenant, d['CM'], d['TD'], d['TP (non dédoublés)'], d['TP (dédoublés)'],
+                         d['Test']))
+                    conn.commit()
+
+
+            else:
+                print("Aucune donnée pour cet intervenant pour le moment.")
+
+            # Insert the resource without intervenant data into the database
+            cursor.execute(
+                "INSERT INTO RecupHProf (Ressource, CM, TD, TP_non_dedoubles, TP_dedoubles, Test) VALUES (?, ?, ?, ?, ?, ?)",
+                (resource, None, None, None, None, None))
+            conn.commit()
+
+            print("\n")
 
 
 # Load the Excel file
@@ -68,4 +103,4 @@ for sheet in sheet_names:
 df_s1 = pd.read_excel(file_path, sheet_name=sheet_names[0])
 
 # Extract and display the resource information
-extract_and_display_resource_info(df_s1)
+extract_and_display_resource_info(df_s1, conn)
