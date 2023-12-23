2491d55eee850aa72ce5c0b9ec279618bdd57d6a
chiheb bradai <chiheb.bradai@etu.univ-amu.fr>
2023-12-23 13:50:20 +0100

    écriture de chaque cours avec la date etc le nombre d'heure

--- SAE_3_01/scribeData.py
+++ SAE_3_01/scribeData.py
@@ -1,6 +1,7 @@
 import sqlite3
 import openpyxl as openpyxl
 import os
+from datetime import datetime
 
 
 class ScribeData:
@@ -31,9 +32,6 @@ class ScribeData:
             except FileNotFoundError:
                 print("Le fichier fichier_vierge.xlsx n'a pas été trouvé.")
                 exit()
-
-
-
             for semester in semesters:
                 print(f"Traitement du semestre : {semester}")
 
@@ -68,10 +66,10 @@ class ScribeData:
                         worksheet.cell(row=2, column=2, value=resource)
                         worksheet.cell(row=2, column=7, value=data_from_database[0][4])
 
-
                     # On récupère les données de la base de données HoraireProf
                     self.cursor.execute(
-                        "SELECT Intervenant, TD, TP_Dedoubles,TP_Non_Dedoubles FROM HoraireProf WHERE ressource = ? AND TD IS NOT NULL AND TP_dedoubles IS NOT NULL AND TP_non_dedoubles IS NOT NULL",
+                        "SELECT Intervenant, TD, TP_Dedoubles,TP_Non_Dedoubles FROM HoraireProf WHERE ressource = ? "
+                        "AND TD IS NOT NULL AND TP_dedoubles IS NOT NULL AND TP_non_dedoubles IS NOT NULL",
                         (resource,))
                     data_from_database = self.cursor.fetchall()
 
@@ -85,7 +83,7 @@ class ScribeData:
                         row_index3 = 28
 
                         for intervenant in data_from_database:
-                            #ceci permet d'écrire les intervenant
+                            # ceci permet d'écrire les intervenant
                             worksheet.cell(row=row_index0, column=1, value=intervenant[0])
                             row_index0 += 1
                             # ceci permet d'écrire les intervenant ainsi que leurs nombre de TD
@@ -101,8 +99,8 @@ class ScribeData:
                             worksheet.cell(row=row_index3, column=2, value=intervenant[3])
                             row_index3 += 1
 
-
-                    # On récupère les noms des intervenants ayant un chiffre dans la colonne CM pour afficher les profs qui font les CM
+                    # On récupère les noms des intervenants ayant un chiffre dans la colonne CM pour afficher les
+                    # profs qui font les CM
                     self.cursor.execute(
                         "SELECT Intervenant,CM FROM HoraireProf WHERE Ressource = ? AND CM IS NOT NULL",
                         (resource,))
@@ -116,64 +114,74 @@ class ScribeData:
                             worksheet.cell(row=row_index, column=1, value=intervenant[0])
                             row_index += 1
 
-
-
                     self.cursor.execute(
-                        "SELECT Date_cours, Type_Cours FROM Cours WHERE Semestre = ? AND Ressource = ?",
+                        "SELECT Date_Cours, Type_Cours FROM Cours WHERE Semestre = ? AND ? LIKE Ressource || '%'",
                         (semester, resource))
                     data_from_database = self.cursor.fetchall()
 
                     if data_from_database:
                         print(
                             f"Données de la base de données Cours ({resource}, {semester}): {data_from_database}")
-
                         date_type_rows = {}
-
-                        for cours_data in data_from_database:
-                            date_cours = cours_data[0]
-                            type_cours = cours_data[1]
-
-                            if type_cours == 'Amphi':
-                                column_index = 2
-                            elif type_cours == 'TD':
-                                column_index = 3
-                            elif type_cours == 'TP':
-                                column_index = 4
-                            else:
-                                column_index = None
-
-                            print(f"Valeurs de date_cours : {date_cours}")
-                            print(f"Valeurs de type_cours : {type_cours}")
-
-                            # Vérifier si la date et le type de cours existent déjà dans le dictionnaire
-                            if (date_cours, type_cours) in date_type_rows:
-                                # Utiliser la ligne existante pour la date et le type de cours
-                                row_index5 = date_type_rows[(date_cours, type_cours)]
-                            else:
-                                # Ajouter une nouvelle ligne pour la nouvelle date et le type de cours
+                        dict_date_cours = {}
+                        for i, cours_actuel in enumerate(data_from_database):
+                            date_actuelle = cours_actuel[0]
+                            type_cours = cours_actuel[1]
+                            if date_actuelle in dict_date_cours:
+                                dict_date_cours[date_actuelle].append(type_cours)
+                            # Ajouter le type de cours à la liste s'il n'est pas déjà présent
+                            if type_cours not in dict_date_cours.get(date_actuelle, []):
+                                dict_date_cours.setdefault(date_actuelle, []).append(type_cours)
+                            # Parcourir les éléments suivants pour vérifier les doublons
+                        print(dict_date_cours)
+                        for cle, types_cours in dict_date_cours.items():
+                            # Vérifier si la date a déjà été traitée
+                            if cle not in date_type_rows:
+                                # Si la date n'a pas encore été traitée, obtenir un nouvel index de ligne
                                 row_index5 = max(date_type_rows.values(), default=34) + 1
-                                date_type_rows[(date_cours, type_cours)] = row_index5
+                                date_type_rows[cle] = row_index5
+                            else:
+                                row_index5 = date_type_rows[cle]
+                            # Écrire la date dans la première colonne de la ligne identifiée
+                            worksheet.cell(row=row_index5, column=1, value=cle)
+                            t_cours_traite = {}
+                            for type_cours in types_cours:
+                                # Vérifie si le type de cours existe déjà dans le dictionnaire
+                                if type_cours in t_cours_traite:
+                                    # Augmente le compteur existant pour ce type de cours
+                                    t_cours_traite[type_cours] += 2
+                                else:
+                                    # Initialise le compteur pour ce type de cours
+                                    t_cours_traite[type_cours] = 2
+                            for type_cours in set(types_cours):
+                                # Concaténer le type de cours avec son compteur
+                                type_cours_et_nombre = f"{type_cours} {t_cours_traite[type_cours]}H"
+
+                                # Choix du column_index en fonction du type de cours
+                                if type_cours == 'Amphi':
+                                    column_index = 2
+                                elif type_cours == 'TD':
+                                    column_index = 3
+                                elif type_cours == 'TP':
+                                    column_index = 4
+                                else:
+                                    continue  # Si le type de cours n'est pas reconnu, passer au suivant
 
                                 # Écrire les données dans la feuille Excel
-                                worksheet.cell(row=row_index5, column=1, value=date_cours)
-                                worksheet.cell(row=row_index5, column=column_index, value=type_cours)
+                                worksheet.cell(row=row_index5, column=column_index, value=type_cours_et_nombre)
 
-                            date_type_rows[(date_cours, type_cours)] += 1
-
-
-
-
-            filename = f"{semestre}.xlsx"
-            save_directory = 'fichiers genere'
-            if not os.path.exists(save_directory):
-                os.makedirs(save_directory)
-            save_path = os.path.join(save_directory, filename)
-            # Sauvegarde du fichier Excel
-            fichier_vierge.save(save_path)
-            print(f"Les données ont été ajoutées à {save_path}")
+                filename = f"{semestre}.xlsx"
+                save_directory = 'fichiers genere'
+                if not os.path.exists(save_directory):
+                    os.makedirs(save_directory)
+                save_path = os.path.join(save_directory, filename)
+                # Sauvegarde du fichier Excel
+                fichier_vierge.save(save_path)
+                print(f"Les données ont été ajoutées à {save_path}")
 
         except sqlite3.Error as e:
             print(f"Erreur lors de la connexion à la base de données : {e}")
+
 
 # appel de la méthode
 scribeData = ScribeData()
