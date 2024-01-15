0dfa0bda38463eb2f757efdcbae5468506c9f9fc
chiheb bradai <chiheb.bradai@etu.univ-amu.fr>
2024-01-15 18:54:34 +0100

    mourir

--- SAE_3_01/selectFile.py
+++ SAE_3_01/selectFile.py
@@ -1,14 +1,11 @@
 import tkinter as tk
 from tkinter import filedialog, messagebox
 import os
-import pandas as pd
-
 
 class selectFile:
     """
     Classe qui permet de faire choisir à l'utilisateur l'endroit où se trouve chaque fichier nécessaire au programme
     """
-    fichier_destination = None
     instance = None
     maquette_BUT1 = None
     maquette_BUT2 = None
@@ -39,15 +36,11 @@ class selectFile:
                 file.write("")
                 self.open_select_file()
 
-    def verify_before_open(self, file):
-        if os.path.exists(file):
-            print("ok")
-            return True
-        else:
-            return False
-
     def open_select_file(self):
-        if os.path.exists(self.fichier_destination):
+        # initialise Tkinter
+        root = tk.Tk()
+        root.withdraw()
+        if os.path.exists("fichiers necessaires/file_destination.txt"):
             open('fichiers necessaires/file_destination.txt', 'w').close()
         self.open_select_file_BUT1_maquette()
         self.open_select_file_BUT2_maquette()
@@ -74,7 +67,7 @@ class selectFile:
         :return:
         """
         # Affiche le message
-        messagebox.showinfo("IMPORTANT", "Veuillez sélectionner le fichier du BUT 2")
+        messagebox.showinfo("IMPORTANT", "Veuillez sélectionner le fichier maquette national du BUT 2")
         # ouvre la fenêtre pour choisir un fichier
         self.maquette_BUT2 = filedialog.askopenfilename()
         # écrit la destination dans le fichier destination
@@ -130,38 +123,24 @@ class selectFile:
         # écrit la destination dans le fichier destination
         self.scribe_destination_file("QFQ", self.QFQ)
 
-    def scribe_destination_file(self, nom_fichier, nouvelle_destination):
+    def scribe_destination_file(self, nom_fichier, destination):
         """
         Écrit dans un fichier .TXT le nom et la destination de chaque fichier
         :param nom_fichier:
-        :param nouvelle_destination:
+        :param destination:
         :return:
         """
-        with open(self.fichier_destination, "r") as file:
-            lignes = file.readlines()
-        print(lignes)
-        is_writed = False
-        # Modifier la ligne correspondante
-        with open(self.fichier_destination, "w") as file:
-            for ligne in lignes:
-                if ligne.startswith(nom_fichier + "="):
-                    # Remplacer par la nouvelle destination
-                    file.write(f"{nom_fichier}={nouvelle_destination}\n")
-                    is_writed = True
-                else:
-                    # Conserver les autres lignes telles quelles
-                    file.write(ligne)
-        if not is_writed:
-            with open(self.fichier_destination, "a") as file:
-                file.write(f"{nom_fichier}={nouvelle_destination}\n")
+        fichier_destination = "fichiers necessaires/file_destination.txt"
+        with open(fichier_destination, "a") as file:
+            file.write(f'{nom_fichier}={destination}\n')
 
     def recup_destination_file(self):
         """
         Récupère la destination de chaque fichier dans le fichier.
         :return:
         """
-        fichiers_erreur = {"BUT1", "BUT2", "BUT3", "planning", "nom_prof", "QFQ"}
-        with open(self.fichier_destination, "r") as fichier:
+        # Ouvre le fichier en mode lecture
+        with open("fichiers necessaires/file_destination.txt", "r") as fichier:
             # Lire chaque ligne du fichier
             for ligne in fichier:
                 # Divise la ligne en utilisant le signe "=" comme séparateur
@@ -169,51 +148,20 @@ class selectFile:
 
                 # Vérifie si la ligne a été correctement divisée en deux parties
                 if len(parties) == 2:
-                    # Récupère chaque chemin de chaque fichier
                     match parties[0]:
                         case "BUT1":
                             self.maquette_BUT1_file = parties[1]
-                            if self.verify_before_open(parties[1]):
-                                fichiers_erreur.remove("BUT1")
                         case "BUT2":
                             self.maquette_BUT2_file = parties[1]
-                            if self.verify_before_open(parties[1]):
-                                fichiers_erreur.remove("BUT2")
                         case "BUT3":
                             self.maquette_BUT3_file = parties[1]
-                            if self.verify_before_open(parties[1]):
-                                fichiers_erreur.remove("BUT3")
                         case "planning":
                             self.planning_file = parties[1]
-                            if self.verify_before_open(parties[1]):
-                                fichiers_erreur.remove("planning")
                         case "nom_prof":
                             self.nom_prof_file = parties[1]
-                            if self.verify_before_open(parties[1]):
-                                fichiers_erreur.remove("nom_prof")
                         case "QFQ":
                             self.QFQ_file = parties[1]
-                            if self.verify_before_open(parties[1]):
-                                fichiers_erreur.remove("QFQ")
                         case _:
                             print("erreur inattendu")
                 else:
                     print(f"Erreur de format sur la ligne : {ligne}")
-        print(fichiers_erreur)
-        for fichiers_cor in fichiers_erreur:
-            print(fichiers_cor)
-            match fichiers_cor:
-                case "BUT1":
-                    self.open_select_file_BUT1_maquette()
-                case "BUT2":
-                    self.open_select_file_BUT2_maquette()
-                case "BUT3":
-                    self.open_select_file_BUT3_maquette()
-                case "planning":
-                    self.open_select_file_planning()
-                case "nom_prof":
-                    self.open_select_file_nom_prof()
-                case "QFQ":
-                    self.open_select_file_QFQ()
-                case _:
-                    print("erreur inattendu")
